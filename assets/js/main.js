52ce4a74da743c6ad2781fcebd0f119050a6d35b
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-12-26 22:30:35 -0300

    att

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
 const menu = document.querySelector('.menu');
+const body = document.querySelector('#body');
+const larguraTela = window.innerWidth;
+
 
 function clickMenu() {
     if (menu.style.display == 'flex') {
-        menu.style.display = 'none'
+        menu.style.display = 'none';
+        body.style.overflowY = 'scroll';
     } else {
-        menu.style.display = 'flex'
+        menu.style.display = 'flex';
+        body.style.overflowY = 'hidden';
     }
 }
 
 function fecharMenu() {
-    menu.style.display = 'none'
+    menu.style.display = 'none';
 }
